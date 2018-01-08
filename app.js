ecedc84fe3262c704cfc48444c51024145f942b4
Cam C <camoson343@github.com>
2018-01-07 22:15:26 -0800

    need checkup

--- app.js
+++ app.js
@@ -1,44 +1,64 @@
 function onReady() {
   let toDos = [];
-  const addToDoForm = document.getElementById('addNewToDoForm')
+  let id = 0;
+  const addToDoForm = document.getElementById('addToDoForm')
   const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
-function createNewToDo(){
-  if (!newToDoText.value) { return; }
+  const newToDo = document.createElement('li');
 
-  toDos.push({
-    title: newToDoText.value,
-    complete: false
-  });
+  function createNewToDo(){
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id++
+    });
+
+    newToDoText.value = '';
+
+    renderTheUI();
+  }
+
+  function renderTheUI(){
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo){
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      const title = document.createElement('span');
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+
+      let deleteButton = document.createElement('button')
+      deleteButton.textContent = 'delete'
+
+      newLi.appendChild(deleteButton)
+
+      deleteButton.addEventListener('click', () => {
+        deleteToDo(toDo.id);
+        renderTheUI();
+
+      });
+
+    });
+  }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
-    newToDoText.vaule = '';
+    newToDoText.value = '';
   });
 
   renderTheUI();
-}
 
-function renderTheUI(){
-  const toDoList = document.getElementById('toDoList');
-
-  toDoList.textContent = '';
-
-  toDos.forEach(function(toDo){
-    const newToDo = document.createElement('li');
-
-       const checkbox = document.createElement('input');
-       checkbox.type = "checkbox";
-
-       const title = document.createElement('span');
-       title.textContent = toDo.title;
-
-       toDoList.appendChild(newLi);
-       newLi.appendChild(checkbox);
-
-  });
-}
+  function deleteToDo(id){
+    toDos.filter[toDos = toDos.filter(item => item.id !==id)]
+  }
 }
 
 window.onload = function() {
